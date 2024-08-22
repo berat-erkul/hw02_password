7561cf370b66a863862118a9c60c3e761722103e
berat-erkul <beraterkul00@gmail.com>
2024-08-22 03:03:09 +0300

    message1

---
+++ src/PasswordTest.java
@@ -0,0 +1,21 @@
+
+public class PasswordTest {
+
+    public static void main(String[] args) {
+
+        Password password=new Password();
+
+        System.out.println(password);
+
+        password.setUsername("adam");
+        System.out.println(password);
+
+        System.out.println("password.getUsername(\"ada\") = " + password.getUsername("ada"));
+
+        password.setPassword("adam","adam123");
+
+        System.out.println(password);
+
+        System.out.println(password.getPassword("adam"));
+    }
+}
